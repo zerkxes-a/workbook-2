039d700398d065ee34fa3902bf3aa8e8db24d06b
zerkxes-a <annazerkle@gmail.com>
2025-04-17 14:36:30 -0400

    some of dice app completed

---
+++ RollTheDice/src/main/java/com/pluralsight/Dice.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+
+public class Dice {
+    private int number;
+
+    public Dice() {
+        this.number = 1;
+    }
+
+    public int roll() {
+        int randomNumber = (int)(Math.random() * 6) + 1;
+        int this.number = randomNumber;
+        int this.number = randomNumber;
+        //whoops probs delete later
+
+
+
+
+
+
+
+    }
+}
+
---
+++ RollTheDice/src/main/java/com/pluralsight/DiceRoller.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class DiceRoller {
+    public static void main(String[] args) {
+        int roll1 =
+        int roll2 =
+
+    }
+}
